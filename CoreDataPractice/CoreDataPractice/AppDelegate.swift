123a7fbf55575daa174d594486e1c152d3128bab
Arsenty Halimovsky <ghost12@yandex.ru>
2023-05-01 20:34:47 +0300

    Rename TaskViewController

--- CoreDataPractice/CoreDataPractice/AppDelegate.swift
+++ CoreDataPractice/CoreDataPractice/AppDelegate.swift
@@ -11,9 +11,14 @@ import CoreData
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = UINavigationController(rootViewController: TaskViewController())
+        
         return true
     }
 
--- CoreDataPractice/CoreDataPractice/TaskViewController.swift
+++ CoreDataPractice/CoreDataPractice/TaskViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TaskViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
